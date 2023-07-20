c4ff66314534976b86c7e4caa093421cf8495d21
nadjib-boum <nadjib.dev21@gmail.com>
2023-07-20 22:03:47 +0400

    improve typing

--- src/playground.ts
+++ src/playground.ts
@@ -2,6 +2,11 @@ import HTTPClient from "./index";
 
 const httpClient = new HTTPClient("https://jsonplaceholder.typicode.com");
 
+type ApiResponse<T = any> = {
+  status: "success";
+  data: T;
+};
+
 async function getUsers() {
   try {
     const users = await httpClient.get("/users");
